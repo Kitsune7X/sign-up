7f38afc5f2180593c40daa51024e7709b189f75b
Kitsune7X <ryuizanagi@proton.me>
2025-11-11 20:49:46 +0900

    complete basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,55 @@ const App = () => {
         </div>
       </div>
 
-      <div>
+      <div className="sign-up-section">
+        <div>
+          <span>
+            The Hokkaido red fox grows a thicker, softer coat each winter — its
+            fur glowing golden even in the deep snow, a beacon of warmth in the
+            cold north.
+          </span>
+        </div>
         <form>
-          <label htmlFor="">
-            Username:
-            <input type="text" />
-          </label>
+          <h2>Sign Up Now!</h2>
+          <section>
+            <div>
+              <label htmlFor="">
+                First name:
+                <input type="text" />
+              </label>
+              <label htmlFor="">
+                Last name:
+                <input type="text" />
+              </label>
+            </div>
+            <div>
+              <label htmlFor="">
+                Email:
+                <input type="text" />
+              </label>
+              <label htmlFor="">
+                Phone number:
+                <input type="text" />
+              </label>
+            </div>
+            <div>
+              <label htmlFor="">
+                Password:
+                <input type="text" />
+              </label>
+              <label htmlFor="">
+                Confirm Password:
+                <input type="text" />
+              </label>
+            </div>
+          </section>
+
+          <section>
+            <button type="submit">Create an Account</button>
+            <p>
+              Already have an Account ? <a href="#">Log in</a>
+            </p>
+          </section>
         </form>
       </div>
     </div>
